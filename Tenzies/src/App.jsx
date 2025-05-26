60f791d4cf59998f724ad0597d58112ce20a1c2c
syessaching <syessa.ching@gmail.com>
2025-05-26 10:58:58 -1000

    added confetti when user won the game

--- Tenzies/src/App.jsx
+++ Tenzies/src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import Die from './Die.jsx'
 import {nanoid} from "nanoid"
 import { useEffect } from 'react'
+import Confetti from 'react-confetti'
+import { useWindowSize } from '@react-hook/window-size';
+
 
 
 function App() {
   const [count, setCount] = useState(0)
   const [dice, setDice]= useState(generateAllNewDice())
   const [gameWon, setGameWon] = useState(false)
+  const [width, height] = useWindowSize ()
 
   useEffect(() => {
     const allHeld = dice.every(die => die.isHeld)
@@ -66,6 +70,7 @@ function App() {
 
   return (
     <>
+      {gameWon && <Confetti width={width} height={height} />}
       <main> 
         <h1 className="title">Tenzies</h1>
         <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
