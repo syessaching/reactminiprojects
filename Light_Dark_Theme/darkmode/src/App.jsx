e79df61d6b0a8cc946d53c23f9c3d91810230433
syessaching <syessa.ching@gmail.com>
2025-06-08 21:08:10 -1000

    added useEffect and useState so logic is applied in toggling the light and dark button

--- Light_Dark_Theme/darkmode/src/App.jsx
+++ Light_Dark_Theme/darkmode/src/App.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import { LuSun } from "react-icons/lu";
 import { LuMoon } from "react-icons/lu";
+import { useEffect } from "react";
+
 
 
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    document.documentElement.className = theme === "dark" ? "dark" : "";
+  }, [theme]);
+
   return(
-    <div className="bg-white dark:bg-zinc-800 grid place-items-center h-screen w-full dark">
+    <div className="bg-white dark:bg-zinc-800 grid place-items-center h-screen w-full ">
       <div className="bg-zinc-100 dark:bg-zinc-700 p-2 rounded-xl">
-        <button className="bg-transparent p-3 hover:bg-zinc-200  dark:hover:bg-zinc-100/10 rounded-lg text-black dark:text-white">
+        <button 
+          onClick={()=>{
+            setTheme("light");
+          }}
+        className="bg-transparent p-3 hover:bg-zinc-200  dark:hover:bg-zinc-100/10 rounded-lg text-black dark:text-white">
         <LuSun />
         </button>
-        <button className="bg-transparent p-3 hover:bg-zinc-200  dark:hover:bg-zinc-100/10 rounded-lg text-black dark:text-white">
+        <button 
+          onClick={()=>{
+            setTheme("dark");
+          }}
+        className="bg-transparent p-3 hover:bg-zinc-200  dark:hover:bg-zinc-100/10 rounded-lg text-black dark:text-white">
         <LuMoon />
         </button>
 
